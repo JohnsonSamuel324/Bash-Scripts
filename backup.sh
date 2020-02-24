5a1e315ccbf0eb145b6555a4d7510e805c3f7187
Samuel Johnson <samuel.johnson@ecolab.com>
2020-02-24 12:18:57 -0600

    Created script for backup - LINUX

--- backup.sh
+++ backup.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 User=$(whoami)
-Input="/home/Users/johnss2/documents"
-Output="/home/Users/johnss2/desktop/$Filename"
-Filename=ug-$(date +%-Y-m%-d)-$(date +%-T).tar.gz
-tar -czf $Output $Input
+input="/home/$User/"
+output="/home/$User/Backups/"
+fileName="Backup"-$(date +%b-%d-%Y)-$(date +%I:%M)
+
+if [ ! -d "$output" ]
+then
+	echo "Making backups directory..."
+	mkdir "$output"
+fi
+
+oldFile="Backup-*"
+cd "$output"
+if [ -d "$oldFile" ] && [ ! -d "$output$fileName" ]
+then
+	echo "Old backup found..."
+	echo "Overwriting old backup..."
+	mv $oldFile $output$fileName
+else
+	echo "No old backup..."
+	echo "Creating new backup..."
+fi
+cd ..
+
+rsync -azh --stats $input $output$fileName --delete --exclude Backups -- .cache --exclude Trash
