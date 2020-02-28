355592cb07b4c572aea310ef8f337f97b6c798f2
Samuel Johnson <JohnsonSamuel324@gmail.com>
2020-02-28 14:07:45 -0600

    Delete startup.sh

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## This script is used to run the startup scripts one at a time on bootup so they won't overlap in the logs.txt file
-
-# Runs the update script
-./update.sh
-# Runs the backup script
-./backup.sh
-
-# -Suspect
